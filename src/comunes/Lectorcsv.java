d80efb8b85372ae10b3551c7a682bc0d35aa0195
in2dam-b <in2dam-b@HZ061512.elorrieta.int>
2020-10-07 20:23:19 +0200

    Commit con las bases del proyecto.

---
+++ src/comunes/Lectorcsv.java
@@ -0,0 +1,76 @@
+package comunes;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import modelo.Libro;
+
+public class Lectorcsv {
+	
+	public static final String SEPARATOR=";";
+	public static final String QUOTE="\"";
+	   
+	public static void lectorcsv() {
+		
+		 BufferedReader br = null;
+		 int cantidad = 0;
+	      
+	      try {
+			try {
+			     
+			     br =new BufferedReader(new FileReader("listalibros.csv"));
+			     String line = br.readLine();
+			     while (null!=line) {
+			        String [] fields = line.split(SEPARATOR);			        
+			        fields = removeTrailingQuotes(fields);
+			        
+			        if (!fields[0].equals("Título")){
+			        	cantidad = cantidad + 1;
+			        	fields = removeTrailingQuotes(fields);
+			        	Libro libro = new Libro();
+				        libro.setTitulo(fields[0]);
+				        libro.setEditorial(fields[1]);
+				        libro.setPaginas(Integer.valueOf(fields[2]));
+				        libro.setAltura(Integer.valueOf(fields[3]));
+				        libro.setNotas(fields[4]);
+				        libro.setIsbn(fields[5]);
+				        libro.setMaterias(fields[6]);
+				        
+				        System.out.println("Libro " + cantidad);
+				        System.out.println("\tTítulo: " + libro.getTitulo());	
+				        System.out.println("\tPáginas: " + libro.getPaginas());
+				        System.out.println("\tAltura: " + libro.getAltura());
+				        System.out.println("\tNotas: " + libro.getNotas());
+				        System.out.println("\tISBN: " + libro.getIsbn());
+				        System.out.println("\tMaterias: " + libro.getMaterias());
+				
+			        }
+			        
+			        line = br.readLine();
+			     }
+			     
+			  } catch (Exception e) {
+
+			  } finally {
+			     if (null!=br) {
+			        br.close();
+			     }
+			  }
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	      
+	}
+	
+	private static String[] removeTrailingQuotes(String[] fields) {
+
+	      String result[] = new String[fields.length];
+
+	      for (int i=0;i<result.length;i++){
+	         result[i] = fields[i].replaceAll(QUOTE, "");
+	      }
+	      return result;
+	   }
+
+}
---
+++ src/principal/Programa.java
@@ -0,0 +1,67 @@
+package principal;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import comunes.Lectorcsv;
+
+public class Programa {
+	
+	public static Scanner teclado = new Scanner (System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int opcion = 0;
+		
+		System.out.println("¿Qué tipo de fichero quiere leer?");
+		
+		do {
+			
+			System.out.println();
+			System.out.println("Introduzca 1 para leer un archivo .txt");
+			System.out.println("Introduzca 2 para leer un archivo .xml");
+			System.out.println("Introduzca 3 para leer un archivo .csv");
+			System.out.println();
+			System.out.print("Elija su opción: ");
+			
+			try {
+				opcion = teclado.nextInt();
+				
+			} catch (InputMismatchException e) {
+				opcion = 0;
+				teclado.nextLine();
+			}
+			
+			if (opcion != 1 && opcion != 2 && opcion != 3) {
+				
+				System.out.println();
+				System.out.println("Opción incorrecta, vuelva a elegir");
+			}
+			
+			System.out.println();
+			
+			switch (opcion) {
+			case 1:
+				
+				break;
+				
+			case 2:
+				
+				break;
+			
+			case 3:
+				
+				Lectorcsv.lectorcsv();
+				
+				break;
+
+			default:
+				break;
+			}
+			
+		} while (opcion != 1 && opcion != 2 && opcion != 3);
+		
+	}
+
+}
